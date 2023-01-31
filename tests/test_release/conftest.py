bd6a938cc152c21166909d7ca1a47ebb51895c91
Constabtin_bunker <ka.mikhailo.1984@gmail.com>
2023-01-31 12:01:06 +0300

    Изменил и убрал токен к github из доступа

--- tests/test_release/conftest.py
+++ tests/test_release/conftest.py
@@ -4,11 +4,17 @@ import requests
 import repmaker as app
 
 
+import os
+
+
 @pytest.fixture(scope="session")
 def github():
     '''Соединение с github'''
     username = "ConsMikh"
-    token = "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"
+    dir = 'tests\\test_release'
+    path = os.path.join(dir, 'token.txt')
+    with open(path, 'r') as f:
+        token = f.readline()
 
     gh_session = requests.Session()
     gh_session.auth = (username, token)
